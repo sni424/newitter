044bbf16db90f9d6e3bd3a4406138f8af5d07bc6
Your Name <you@example.com>
2022-02-13 23:53:19 +0900

    22-02-13

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,11 @@
 import React from "react";
+import { Router } from "react-router-dom";
+import Rout from "./Router";
 
 function App() {
   return (
     <div>
+      <Rout></Rout>
     </div>
   );
 }
---
+++ src/components/Router.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import { Router, Routes, Route } from "react-router-dom";
+import Auth from "../routes/Auth";
+import Home from "../routes/Home";
+
+function Rout() {
+
+    const [isLoggedIn, setIsLoggedIn] = useState(true);
+    return (
+        <>
+            <Routes>
+                {isLoggedIn
+                    ? <Route exact path="/" element={<Home></Home>}>
+                    </Route>
+                    : <Route exact path="/" element={<Auth></Auth>}>
+                    </Route>
+                }
+            </Routes>
+        </>
+    )
+}
+
+export default Rout;
